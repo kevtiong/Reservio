0c5c87ca3c41dbee272d38696ccf904b91fb0596
kevtiong <126160399+kevtiong@users.noreply.github.com>
2024-04-18 22:54:04 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,23 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from './components/Home.vue';
-import Restaurants from './components/Restaurants.vue';
-import FAQs from './components/FAQs.vue';
-import Contact from './components/Contact.vue';
+import Home from '../components/Home.vue'; // Adjust import paths for components
+import Contact from '../components/Contact.vue'; // Adjust import paths for components
+import FAQs from '../components/FAQs.vue'; // Adjust import paths for components
+import Restaurants from '../components/Restaurants.vue'; // Adjust import paths for components
 
 const routes = [
   { path: '/', component: Home },
   { path: '/restaurants', component: Restaurants },
-  { path: '/faqs', component: FAQs },
+  { path: '/faq', component: FAQs },
   { path: '/contact', component: Contact },
- ];
+];
 
 const router = createRouter({
- history: createWebHistory(),
- routes,
+  history: createWebHistory(),
+  routes,
 });
 
-createApp(Home).use(router).mount('#app');
+export default router;
+
+
+createApp(App).use(router).mount('#app');
