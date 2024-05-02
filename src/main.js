37d6c9e8cea339bc7dead8036c4b4cad52003b0d
kevtiong <126160399+kevtiong@users.noreply.github.com>
2024-05-02 12:12:35 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import Home from './components/Home.vue'
+import { createApp } from 'vue';
+import App from './App.vue'; // Import the App.vue file
+import router from './router'; // Import your router configuration
 
-
-
-createApp(Home).mount('#app')
+createApp(App) // Create a new Vue app instance with App.vue as the root component
+  .use(router) // Use the router configuration
+  .mount('#app'); // Mount the app to the '#app' element in index.html
